c2c5c9f63af8b3614551c0c4789e6b762b44e945
Daisy Kirui <daisykkirui@gmail.com>
2022-06-18 18:05:27 +0300

    Adding the db.json file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let inputBox = document.getElementById("create-task-form");
+  inputBox.addEventListener("submit", function(e){
+    e.preventDefault() 
+    myList(e.target.label)
+    inputBox.reset()
+  })
 });
+
+function myList(myTask){
+ 
+  let ul = document.querySelector("tasks")
+  let li = document.createElement("li")
+  let btn = document.createElement("button")
+  btn.addEventListener("click", erase)
+  btn.textContent = "delete" 
+  li.textContent = `${myTask}`
+  ul.appendChild(li)
+  ul.appendChild(btn)
+
+  console.log(li)
+  
+}
+
+function erase (e){
+  e.target.ul.remove
+
+}
+
+
+
+
